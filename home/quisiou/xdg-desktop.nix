cf054c8c1872498a8e6a8c0142f7deff6a5c3c45
QuiSioU <marco.casteleiro@gmail.com>
2026-06-27 16:01:46 +0200

    Added some more app desktop files to xdg config

--- home/quisiou/xdg-desktop.nix
+++ home/quisiou/xdg-desktop.nix
@@ -13,11 +13,80 @@
             icon = "btop";
             exec = "kitty -e btop";
             terminal = true;
-            categories = [ "System" "Monitor" "ConsoleOnly" ];
+            categories = [
+                "System"
+                "Monitor"
+                "ConsoleOnly"
+            ];
             settings = {
-                Version = "1.0";
                 Keywords = "system;process;task";
             };
+        };
+        nvim = {
+            type = "Application";
+            name = "Neovim";
+            genericName = "Text Editor";
+            comment = "Edit text files";
+            icon = "nvim";
+            exec = "kitty -e nvim %F";
+            terminal = true;
+            startupNotify = false;
+            categories = [
+                "Utility"
+                "TextEditor"
+                "Development"
+            ];
+            settings = {
+                Keywords = "Text;editor;vim;nvim";
+            };
+        };
+        "org.musescore.MuseScore" = {
+            type = "Application";
+            name = "MuseScore Studio";
+            genericName = "Music Notation";
+            comment = "Create, play and print beautiful sheet music";
+            icon = "mscore";
+            exec = "env DESKTOPINTEGRATION=false QT_SCALE_FACTOR=1.5 QT_QPA_PLATFORM=wayland mscore %U";
+            terminal = false;
+            startupNotify = true;
+            categories = [
+                "AudioVideo"
+                "Audio"
+                "Graphics"
+                "2DGraphics"
+                "VectorGraphics"
+                "RasterGraphics"
+                "Publishing"
+                "Midi"
+                "Mixer"
+                "Sequencer"
+                "Music"
+                "Qt"
+            ];
+            settings = {
+                Keywords = "music;notation;composition;composing;arranging;making;sheet music;music notation software;lead sheet;leadsheet;score;full score;scorewriter;MIDI;musicxml;playback;instrument";
+            };
+        };
+        umpv = {
+            type = "Application";
+            name = "MPV Media Player";
+            genericName = "Multimedia player";
+            comment = "Play movies and songs";
+            icon = "mpv";
+            exec = "umpv %U";
+            terminal = false;
+            startupNotify = false;
+            categories = [
+                "AudioVideo"
+                "Audio"
+                "Video"
+                "Player"
+                "TV"
+            ];
+            settings = {
+                Keywords = "umpv;media;player;video;audio;tv";
+            };
+            noDisplay = true;
         };
     };
 }
