7808c64fcfd6f977931973a993eda17f51e55d74
QuiSioU <marco.casteleiro@gmail.com>
2026-06-30 12:59:05 +0200

    Added tree command utility

--- home/quisiou/packages.nix
+++ home/quisiou/packages.nix
@@ -6,7 +6,7 @@
 {
     home.packages = with pkgs; [
         # Basic terminal utilities
-        bat duf dust ffmpeg jq poppler fd ripgrep fzf zoxide resvg imagemagick
+        bat duf dust tree ffmpeg jq poppler fd ripgrep fzf zoxide resvg imagemagick
         _7zz-rar
 
         # Text editors
